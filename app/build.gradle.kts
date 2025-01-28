3c40d012f45cda64509962571faf41c1a9a4b16c
立謙 <llc890410@gmail.com>
2025-01-28 18:54:32 +0800

    專案導入 Dagger Hilt

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.hilt.android.gradle.plugin)
+    alias(libs.plugins.ksp)
 }
 
 android {
@@ -49,6 +51,8 @@ dependencies {
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+    implementation(libs.hilt.android)
+    ksp(libs.hilt.ksp)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
---
+++ app/src/main/java/com/michaelliu/twsestockinfo/TWSEStockInfoApp.kt
@@ -0,0 +1,9 @@
+package com.michaelliu.twsestockinfo
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class TWSEStockInfoApp: Application() {
+
+}
