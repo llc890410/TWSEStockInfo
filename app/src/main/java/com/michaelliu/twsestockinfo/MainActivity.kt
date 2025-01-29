af4b0a52dc77e5c68fa51c16a3044912ee2b0ef5
立謙 <llc890410@gmail.com>
2025-01-29 21:35:00 +0800

    畫面執行API請求

--- app/src/main/java/com/michaelliu/twsestockinfo/MainActivity.kt
+++ app/src/main/java/com/michaelliu/twsestockinfo/MainActivity.kt
@@ -2,11 +2,20 @@ package com.michaelliu.twsestockinfo
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.michaelliu.twsestockinfo.presentation.ui.stocklist.StockInfoListViewModel
+import com.michaelliu.twsestockinfo.utils.NetworkResult
+import dagger.hilt.android.AndroidEntryPoint
+import timber.log.Timber
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    private val stockInfoListViewModel: StockInfoListViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -15,6 +24,22 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        observeStockState()
+        stockInfoListViewModel.fetchStockInfoList()
+    }
+
+    private fun observeStockState() {
+        stockInfoListViewModel.stockState.observe(this) { state ->
+            when (state) {
+                is NetworkResult.Success -> {
+                    Timber.e("Stock list: ${state.data}")
+                }
+                is NetworkResult.Failure -> {
+                    Timber.e("Error: ${state.exception}")
+                }
+            }
         }
     }
 }
---
+++ app/src/main/java/com/michaelliu/twsestockinfo/presentation/ui/stocklist/StockInfoListViewModel.kt
@@ -0,0 +1,27 @@
+package com.michaelliu.twsestockinfo.presentation.ui.stocklist
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.michaelliu.twsestockinfo.domain.model.StockInfo
+import com.michaelliu.twsestockinfo.domain.usecase.GetStockInfoListUseCase
+import com.michaelliu.twsestockinfo.utils.NetworkResult
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class StockInfoListViewModel @Inject constructor(
+    private val getStockInfoListUseCase: GetStockInfoListUseCase
+) : ViewModel() {
+    private val _stockState = MutableLiveData<NetworkResult<List<StockInfo>>>()
+    val stockState: LiveData<NetworkResult<List<StockInfo>>> = _stockState
+
+    fun fetchStockInfoList() {
+        viewModelScope.launch {
+            val result = getStockInfoListUseCase()
+            _stockState.value = result
+        }
+    }
+}
